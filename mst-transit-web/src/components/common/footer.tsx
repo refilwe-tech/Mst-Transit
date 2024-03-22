a6a712f625fdc64f0cc08668facad2866fc243ec
refilwe-m <refilwe.dev@gmail.com>
2024-03-22 02:39:50 +0200

    Active business

--- mst-transit-web/src/components/common/footer.tsx
+++ mst-transit-web/src/components/common/footer.tsx
@@ -1,12 +1,25 @@
 import { IoHomeOutline, IoHome } from "react-icons/io5";
 import { HiOutlineClipboardList, HiClipboardList } from "react-icons/hi";
-import { FaMotorcycle, FaRegHandshake } from "react-icons/fa";
-import { MdAddBusiness, MdOutlineAddBusiness } from "react-icons/md";
-import { FaHandshake } from "react-icons/fa6";
+import { LuPhoneCall } from "react-icons/lu";
+import { BiSolidPhoneCall } from "react-icons/bi";
+
+import {
+  MdAddBusiness,
+  MdOutlineAddBusiness,
+  MdOutlineDeliveryDining,
+} from "react-icons/md";
 import { BsPersonPlusFill, BsPersonPlus } from "react-icons/bs";
 import { ROUTES, ROUTE_IDS } from "../../contants";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 export const Footer = () => {
+  const { pathname, hash } = useLocation();
+  const active = (path: string) =>
+    hash === path || pathname === path
+      ? "font-semibold"
+      : "hover:text-secondary";
+
+  const isActive = (path: string) => hash === path || pathname === path;
+
   return (
     <footer className="bottom-0 w-full sticky flex flex-col items-center gap-2 md:hidden ">
       <section className="flex items-center gap-2 bg-white drop-shadow-lg w-full justify-between px-5">
@@ -14,32 +27,65 @@ export const Footer = () => {
           to={ROUTE_IDS.aboutUs}
           className="p-2 flex items-center flex-col gap-1"
         >
-          <IoHomeOutline size={20} />
-          <p className="text-xs text-center">Home</p>
+          {isActive(ROUTE_IDS.aboutUs) ? (
+            <IoHome size={25} />
+          ) : (
+            <IoHomeOutline size={25} />
+          )}
+          <p className={`${active(ROUTE_IDS.aboutUs)} text-xs text-center"`}>
+            Home
+          </p>
         </Link>
         <Link
           to={ROUTE_IDS.services}
           className="p-2 flex items-center flex-col gap-1"
         >
-          <HiOutlineClipboardList size={20} />
-          <p className="text-xs text-center">Services</p>
+          {isActive(ROUTE_IDS.services) ? (
+            <HiClipboardList size={25} />
+          ) : (
+            <HiOutlineClipboardList size={25} />
+          )}
+          <p className={`${active(ROUTE_IDS.services)} text-xs text-center`}>
+            Services
+          </p>
         </Link>
         <Link
           to={ROUTE_IDS.businesses}
           className="p-2 flex items-center flex-col gap-1"
         >
-          <MdOutlineAddBusiness size={20} />
-          <p className="text-xs text-center">Partners</p>
+          {isActive(ROUTE_IDS.businesses) ? (
+            <MdAddBusiness size={25} />
+          ) : (
+            <MdOutlineAddBusiness size={25} />
+          )}
+          <p className={`${active(ROUTE_IDS.businesses)} text-xs text-center`}>
+            Partners
+          </p>
         </Link>
         <Link
           to={ROUTES.partners}
           className="p-2 flex items-center flex-col gap-1"
         >
-          <BsPersonPlus size={20} />
+          {isActive(ROUTES.partners) ? (
+            <BsPersonPlusFill size={25} />
+          ) : (
+            <BsPersonPlus size={25} />
+          )}
           <p className="text-xs text-center">Add Business</p>
         </Link>
+        <Link
+          to={ROUTE_IDS.contact}
+          className="p-2 flex items-center flex-col gap-1"
+        >
+          {isActive(ROUTE_IDS.contact) ? (
+            <BiSolidPhoneCall size={25} />
+          ) : (
+            <LuPhoneCall size={25} />
+          )}
+          <p className="text-xs text-center">Contact</p>
+        </Link>
         <button className="p-2 flex items-center flex-col gap-1">
-          <FaMotorcycle size={25} />
+          <MdOutlineDeliveryDining size={25} />
           <p className="text-xs text-center">Delivery</p>
         </button>
       </section>
